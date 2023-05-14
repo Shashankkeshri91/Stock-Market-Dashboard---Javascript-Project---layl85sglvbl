aee3bf8735ce4a968e537d11a977daf7e4c941fe
Shashank Keshri <shashankkeshri91109@gmail.com>
2023-05-14 16:38:15 +0530

    Master

--- App.js
+++ App.js
@@ -1,1 +1,7 @@
-
+function fetchAPIExample() {
+    let promiseObject = fetch("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=MPC52ZYBPWS0HSTW");
+    promiseObject
+      .then((response) => response.json())
+      .then((response) => console.log("response -> ", response));
+  }
+  fetchAPIExample();
